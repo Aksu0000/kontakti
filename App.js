c247f4c31e8e7a0add18c93a1e629739151aef43
Aleksi Ekman <a1303599@myy.haaga-helia.fi>
2026-03-16 20:20:49 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+import * as Contacts from "expo-contacts";
+//import * as SMS from "expo-sms";
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+
+    if (status === "granted") {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers],
+      });
+
+      if (data.length > 0) {
+        setContacts(data);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={contacts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          const phone =
+            item.phoneNumbers && item.phoneNumbers.length > 0
+              ? item.phoneNumbers[0].number
+              : "No phone number";
+
+          return (
+            <View style={styles.item}>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text>{phone}</Text>
+            </View>
+          );
+        }}
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Get Contacts" onPress={getContacts} />
+      </View>
     </View>
   );
 }
@@ -13,8 +49,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 100,
+    paddingHorizontal: 20,
+  },
+  item: {
+    padding: 10,
+    borderBottomWidth: 1,
+  },
+  name: {
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 40,
   },
 });
